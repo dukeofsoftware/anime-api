db579c23bb4bdbb4175531525d238249146a3480
DukeEmree <kozanfurkanemre@gmail.com>
2022-09-15 19:03:59 +0300

    adding dist subtree

---
+++ dist/_nuxt/static/1663257576/sfw/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fanime-api\u002F_nuxt\u002Fstatic\u002F1663257576",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsfw",config:{_app:{basePath:"\u002Fanime-api\u002F",assetsPath:"\u002Fanime-api\u002F_nuxt\u002F",cdnURL:a}}}}(null));
